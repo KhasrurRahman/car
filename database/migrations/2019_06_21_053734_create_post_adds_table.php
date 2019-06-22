03c0c88077f1bfb80055bef4cdebb3a31527cac2
KhasrurRahman <khasrur8@gmail.com>
2019-06-23 00:35:52 +0600

    day_4

--- database/migrations/2019_06_21_053734_create_post_adds_table.php
+++ database/migrations/2019_06_21_053734_create_post_adds_table.php
@@ -15,6 +15,45 @@ class CreatePostAddsTable extends Migration
     {
         Schema::create('post_adds', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->string('category');
+            $table->string('make');
+            $table->string('model');
+            $table->string('date_of_first_registration');
+            $table->string('power');
+            $table->integer('kilometer');
+            $table->integer('vehicle_no');
+            $table->string('vehicle');
+            $table->integer('price');
+            $table->integer('vat');
+            $table->string('Perm_gvw');
+            $table->string('Admissible_haulage_weight');
+            $table->string('payload');
+            $table->integer('empty_weight');
+            $table->string('gearing_type');
+            $table->string('brakes');
+            $table->string('suspension');
+            $table->string('axle_configuration');
+            $table->integer('wheels_distance');
+            $table->integer('tyre_size');
+            $table->string('CO2_Emmision_combined');
+            $table->string('Fuel_consumption_combined');
+            $table->string('urban');
+            $table->string('rural');
+            $table->string('tuv_certified');
+            $table->string('Colour');
+            $table->string('Cab');
+            $table->string('Attachment_parts');
+            $table->string('Other_features');
+            $table->string('Further_information');
+            $table->string('image_1')->nullable();
+            $table->string('image_2')->nullable();
+            $table->string('image_3')->nullable();
+            $table->string('image_4')->nullable();
+            $table->string('image_5')->nullable();
             $table->timestamps();
         });
     }
